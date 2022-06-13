b7a428d529cbd296ea8b9cfe0a636ae82ab224fb
asxz0 <asxz0121@gmail.com>
2022-06-13 19:07:20 +0800

    modify MainActivity

--- app/src/main/java/com/jordan/jetpacknavigation/MainActivity.kt
+++ app/src/main/java/com/jordan/jetpacknavigation/MainActivity.kt
@@ -6,10 +6,6 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.activity.viewModels
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.jordan.jetpacknavigation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,66 +14,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*binding.composeCalculateButton.apply {
-            val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-                R.id.option_twenty_percent -> 0.20
-                R.id.option_eighteen_percent -> 0.18
-                else -> 0.15
-            }
-
-            val isChecked = binding.roundUpSwitch.isChecked
-
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                Button(
-                    onClick = {
-                        viewModel.calculateTip(
-                            binding.costOfServiceEditText.text.toString(),
-                            tipPercentage,
-                            isChecked
-                        )
-                    }
-                ) {
-                    Text(text = "Calculate")
-                }
-            }
-        }
-
-        binding.calculateButton.setOnClickListener {
-
-            val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-                R.id.option_twenty_percent -> 0.20
-                R.id.option_eighteen_percent -> 0.18
-                else -> 0.15
-            }
-
-            val isChecked = binding.roundUpSwitch.isChecked
-
-            viewModel.calculateTip(
-                binding.costOfServiceEditText.text.toString(),
-                tipPercentage,
-                isChecked
-            )
-        }
-
-        binding.costOfService.setOnKeyListener { view, keyCode, _ ->
-            handleKeyEvent(view, keyCode)
-        }
-
-        subscribeToObservables()*/
-    }
-
-    /*private fun subscribeToObservables() {
-        viewModel.tipResult.observe(this) {
-            binding.tipResult.text = it
-        }
     }
 
     private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
@@ -88,5 +29,5 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return false
-    }*/
+    }
 }
