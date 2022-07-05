36ecf2a13a506bb2d4b9577d1dd1c22d8a2eb432
asxz0 <asxz0121@gmail.com>
2022-07-05 22:29:03 +0800

    Toggle Button

--- app/src/main/java/com/jordan/jetpacknavigation/presentation/button_screen/ButtonFragment.kt
+++ app/src/main/java/com/jordan/jetpacknavigation/presentation/button_screen/ButtonFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.jordan.jetpacknavigation.R
 import com.jordan.jetpacknavigation.databinding.FragmentButtonBinding
 
@@ -21,8 +22,24 @@ class ButtonFragment : Fragment(R.layout.fragment_button) {
     ): View? {
         _binding = FragmentButtonBinding.inflate(inflater, container, false)
 
+        binding.toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btnAndroid -> showToast("This is Android")
+                    R.id.btnFlutter -> showToast("This is Flutter")
+                    R.id.btnWeb -> showToast("This is Web")
+                }
+            } else {
+                if (toggleButtonGroup.checkedButtonId == View.NO_ID) {
+                    showToast("Nothing Selected")
+                }
+            }
+        }
+
         return binding.root
     }
 
-
+    private fun showToast(str: String) {
+        Toast.makeText(requireContext(), str, Toast.LENGTH_SHORT).show()
+    }
 }
