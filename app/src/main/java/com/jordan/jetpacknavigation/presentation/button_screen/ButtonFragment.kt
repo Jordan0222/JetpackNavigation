2a083cf0c2a00389f94d3106892a13793b3aa48c
asxz0 <asxz0121@gmail.com>
2022-07-19 13:34:28 +0800

    modify notification

--- app/src/main/java/com/jordan/jetpacknavigation/presentation/button_screen/ButtonFragment.kt
+++ app/src/main/java/com/jordan/jetpacknavigation/presentation/button_screen/ButtonFragment.kt
@@ -1,26 +1,65 @@
 package com.jordan.jetpacknavigation.presentation.button_screen
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.jordan.jetpacknavigation.MainActivity
 import com.jordan.jetpacknavigation.R
 import com.jordan.jetpacknavigation.databinding.FragmentButtonBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ButtonFragment : Fragment(R.layout.fragment_button) {
-
 
     private var _binding: FragmentButtonBinding? = null
     private val binding: FragmentButtonBinding
         get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentButtonBinding.inflate(inflater, container, false)
+
+        createNotificationChannel()
+
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(requireContext()).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+
+        val notification = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+            .setContentTitle("Awesome notification")
+            .setContentText("This is content text")
+            .setSmallIcon(R.drawable.ic_home)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(requireContext())
+
+        binding.outlinedButton.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
 
         binding.toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
             if (isChecked) {
@@ -39,7 +78,28 @@ class ButtonFragment : Fragment(R.layout.fragment_button) {
         return binding.root
     }
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
     private fun showToast(str: String) {
         Toast.makeText(requireContext(), str, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        const val CHANNEL_ID = "channel_id"
+        const val CHANNEL_NAME = "channel_name"
+        const val NOTIFICATION_ID = 0
     }
 }
